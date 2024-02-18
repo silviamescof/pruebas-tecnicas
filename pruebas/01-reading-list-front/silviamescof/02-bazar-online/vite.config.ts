c34bd34abbaf0179e31041b761bc706374a86d4a
Silvia Mesa Cofrades <120189826+silviamescof@users.noreply.github.com>
2024-02-18 18:20:39 +0100

    configuracion netlify

--- pruebas/01-reading-list-front/silviamescof/02-bazar-online/vite.config.ts
+++ pruebas/01-reading-list-front/silviamescof/02-bazar-online/vite.config.ts
@@ -1,16 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist', // Directorio de salida después de construir
+    assetsDir: '', // Directorio de activos (assets) relativo al directorio de salida (opcional)
+  },
+});
